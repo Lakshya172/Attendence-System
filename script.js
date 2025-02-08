6c1a0399284136be0ca59d8ffd34f50eed0a9241
Lakshya Agarwal <agarwallakshya172@gmail.com>
2025-02-09 03:42:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('mark-attendance').addEventListener('click', function() {
+    alert('Mark Attendance button clicked!');
+    // Here you can add the logic to mark attendance
+});
+
+document.getElementById('new-registration').addEventListener('click', function() {
+    alert('New Registration button clicked!');
+    // Here you can add the logic for new registration
+});
